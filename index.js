e8a21885e28fa6bc80dfd3c31e1789cff6bb8d6e
Blessing <91621514+BlessingJeff@users.noreply.github.com>
2022-02-01 14:42:48 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express')
+const app = express();
+app.get("/create", (req,res) =>{
+var date = new Date();
+var currentTime = date.getTime();
+var timestamp = currentTime.toString();
+var dateAndTime = `${date.getDate()}-${(date.getMonth()+1)}-${date.getFullYear()}-${date.getHours()}-${date.getMinutes()}`;
+var finalTime = (`${dateAndTime} ${".txt"}`);
+  fs.writeFile(`./textfiles/${finalTime}`, timestamp, function (err){
+    if(err) throw err;
+    console.log("file created succesfully")
+})
+})
+app.get("/files", (req,res) =>{
+    fs.readdir("./textfiles", function(err, files) {
+        if(err) throw err;
+        res.json({files})
+        console.log(files)
+    })
+})
+
+app.listen(3000)
